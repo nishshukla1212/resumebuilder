6f03872b268d707e7ed3693b152d68194b80122c
Nish Shukla <nishith.shukla@arvatosystems.com>
2020-10-04 15:43:49 -0400

    removed backslash from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 npm install -g serverless
 npm install -g serverless-offline
-serverless deploy --stage $env --package \   $CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
+serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
