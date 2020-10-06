ace32f98a581602c8e166a65c305e0b6a073907e
Nish Shukla <nishith.shukla@arvatosystems.com>
2020-10-05 23:02:21 -0400

    added shakespeare db initial method

---
+++ shakespearedb.js
@@ -0,0 +1,66 @@
+const mysql = require('mysql');
+const AWS = require('aws-sdk');
+const path = require('path');
+
+var options = require('./options');
+
+var loginData = {
+  host: options.storageConfig.host,
+  user: options.storageConfig.user,
+  password: options.storageConfig.password,
+  database: options.storageConfig.database
+};
+
+function connect() {
+  return new Promise((resolve, reject) => {
+    let con = mysql.createConnection(loginData);
+    console.log("connecting");
+    con.connect(function (err) {
+      if (err) rejec(err);
+      console.log("Connected!");
+      resolve(con);
+    });
+  });
+}
+
+module.exports.getCharacters = (event, context, callback) => {
+  let resultJSON = {};
+  let resultarr = [];
+  let response = '';
+  let workId = event.queryStringParameters.workId;
+  let queryString = `select distinct ch.CharName from Characters ch ,Works wk where ch.Works = '${workId}'`;
+  connect().then((con) => {
+    con.query(queryString, function (err, result, fields) {
+      if (err) throw err;
+      result.forEach(element => {
+        resultarr.push(element.CharName);
+      });
+      resultJSON.resultarr = resultarr;
+      response = JSON.stringify(resultJSON);
+    });
+    con.end();
+    callback(null, {
+      statusCode: 200,
+      body: response
+    });
+  });
+};
+
+function test() {
+  let workId = 'macbeth';
+  let resultarr = [];
+  let resultJSON = {};
+  let queryString = `select distinct ch.CharName from Characters ch ,Works wk where ch.Works = '${workId}'`;
+  connect().then((con) => {
+    con.query(queryString, function (err, result, fields) {
+      if (err) {con.end(); throw err;}
+      result.forEach(element => {
+        resultarr.push({"name":element.CharName});
+      });
+      resultJSON.resultarr = resultarr;
+      let response = JSON.stringify(resultJSON);
+      con.end();
+    });
+    
+  });
+}
