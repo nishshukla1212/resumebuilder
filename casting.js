f023111ea65b0954afe1c65fba7875dd3eacc8fd
Nish Shukla <nishith.shukla@arvatosystems.com>
2021-01-06 16:55:14 -0500

    cs

--- casting.js
+++ casting.js
@@ -29,35 +29,41 @@ module.exports.insertProfile = (event, context, callback) => {
   let headshot4_url = data[0].headshot4_url;
   let resume_url = data[0].resume_url;
   let demo_reel_url = data[0].demo_reel_url;
+  let selecDeletetProm;
 
   let initialQueryString = `select count(*) as COUNT from submission_profile sp where sp.user_id = '${user_id}'`
   loginDataCasting.getConnection((err, connection) => {
-    connection.query(initialQueryString, function (err, result, fields) {
-      if (err) {responseCode=500;throw err;}
-      result.forEach(element => {
-        console.log(element);
-        if(element.COUNT > 0){
-          let deleteQueryString = `delete from submission_profile where user_id = '${user_id}'`
-          connection.query(deleteQueryString, function (err, result, fields) {
-            if (err) {responseCode=500;throw err;}
-            console.log(result);
-          });
-        }
+
+    selectDeleteProm = new Promise((resolve,reject) => {
+      connection.query(initialQueryString, function (err, result, fields) {
+        if (err) { responseCode = 500; throw err; }
+        result.forEach(element => {
+          console.log(element);
+          if (element.COUNT > 0) {
+            let deleteQueryString = `delete from submission_profile where user_id = '${user_id}'`
+            connection.query(deleteQueryString, function (err, result, fields) {
+              if (err) { responseCode = 500; throw err; }
+              console.log(result);
+            });
+          }
+          resolve();
+        });
       });
     });
 
-  queryString = `Insert into submission_profile (user_id,c_uid, first_name,last_name,email,phone,bio,headshot_url_1,headshot_url_2,headshot_url_3,headshot_url_4,resume_url,demo_reel_url,u_dt,c_dt)`;
-  let valueString = `Values ('${user_id}','${c_uid}','${first_name}','${last_name}','${email}','${phone}','${bio}','${headshot1_url}','${headshot2_url}','${headshot3_url}','${headshot4_url}','${resume_url}','${demo_reel_url}',null,null)`
-  
-  queryString = queryString + ' ' + valueString;
-  console.log(queryString);
- 
-  connection.query(queryString, function (err, result, fields) {
-      if (err) {responseCode=500;throw err;}
-      connection.release();
-      callback(null, {
-        statusCode: responseCode,
-        body: response
+    queryString = `Insert into submission_profile (user_id,c_uid, first_name,last_name,email,phone,bio,headshot_url_1,headshot_url_2,headshot_url_3,headshot_url_4,resume_url,demo_reel_url,u_dt,c_dt)`;
+    let valueString = `Values ('${user_id}','${c_uid}','${first_name}','${last_name}','${email}','${phone}','${bio}','${headshot1_url}','${headshot2_url}','${headshot3_url}','${headshot4_url}','${resume_url}','${demo_reel_url}',null,null)`
+
+    queryString = queryString + ' ' + valueString;
+    console.log(queryString);
+    Promise.all([selecDeletetProm]).then(() => {
+      connection.query(queryString, function (err, result, fields) {
+        if (err) { responseCode = 500; throw err; }
+        connection.release();
+        callback(null, {
+          statusCode: responseCode,
+          body: response
+        });
       });
     });
 
@@ -73,23 +79,23 @@ module.exports.getProfile = (event, context, callback) => {
   console.log(event);
   let userID = event.queryStringParameters.userID;
   let queryString = `select distinct sp.first_name, sp.last_name, sp.email, sp.phone,sp.bio,sp.headshot_url_1,sp.headshot_url_2,sp.headshot_url_3,sp.headshot_url_4,sp.resume_url,sp.demo_reel_url from submission_profile sp where sp.user_id = '${userID}'`;
-  let i=0;
-  loginDataCasting.getConnection((err, connection) =>{
+  let i = 0;
+  loginDataCasting.getConnection((err, connection) => {
     connection.query(queryString, function (err, result, fields) {
-    if (err) {responseCode=500;throw err};
-    result.forEach(element => {
-      resultarr.push({_id: i.toString(), first_name: element.first_name, last_name: element.last_name,email: element.email,phone: element.phone,bio: htmlToString(element.bio).toString(),headshot_url_1: element.headshot_url_1,headshot_url_2: element.headshot_url_2,headshot_url_3: element.headshot_url_3,headshot_url_4: element.headshot_url_4,resume_url: element.resume_url,demo_reel_url: element.demo_reel_url});
-      i++;
-    });
-    resultJSON.resultarr = resultarr;
-    response = JSON.stringify(resultJSON);
-    connection.release();
-    callback(null, {
-      statusCode: responseCode,
-      body: response
+      if (err) { responseCode = 500; throw err };
+      result.forEach(element => {
+        resultarr.push({ _id: i.toString(), first_name: element.first_name, last_name: element.last_name, email: element.email, phone: element.phone, bio: htmlToString(element.bio).toString(), headshot_url_1: element.headshot_url_1, headshot_url_2: element.headshot_url_2, headshot_url_3: element.headshot_url_3, headshot_url_4: element.headshot_url_4, resume_url: element.resume_url, demo_reel_url: element.demo_reel_url });
+        i++;
+      });
+      resultJSON.resultarr = resultarr;
+      response = JSON.stringify(resultJSON);
+      connection.release();
+      callback(null, {
+        statusCode: responseCode,
+        body: response
+      });
     });
   });
-});
 };
 
 var stringToHTML = function (str) {
