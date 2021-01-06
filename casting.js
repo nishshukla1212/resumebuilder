3a3a3204e256fed0034cb9e27e9a3851a910dfdb
Nish Shukla <nishith.shukla@arvatosystems.com>
2021-01-06 10:12:06 -0500

    cs

--- casting.js
+++ casting.js
@@ -48,7 +48,7 @@ module.exports.insertProfile = (event, context, callback) => {
     });
 
   queryString = `Insert into submission_profile (user_id, first_name,last_name,email,phone,bio,headshot_url_1,headshot_url_2,headshot_url_3,headshot_url_4,resume_url,demo_reel_url,u_dt,c_dt)`;
-  let valueString = `Values ('${user_id}','${first_name}','${last_name}','${email}','${phone}',"${bio}",'${headshot1_url}','${headshot2_url}','${headshot3_url}','${headshot4_url}','${resume_url}','${demo_reel_url}','${u_dt}','${c_dt}')`
+  let valueString = `Values ('${user_id}','${first_name}','${last_name}','${email}','${phone}',"${bio}",'${headshot1_url}','${headshot2_url}','${headshot3_url}','${headshot4_url}','${resume_url}','${demo_reel_url}',null,null)`
   
   queryString = queryString + ' ' + valueString;
   console.log(queryString);
