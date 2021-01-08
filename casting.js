1aa653a5f4caa0785ce564c07c785ef84d517636
Nish Shukla <nishith.shukla@arvatosystems.com>
2021-01-08 14:30:29 -0500

    cs html

--- casting.js
+++ casting.js
@@ -159,8 +159,11 @@ module.exports.getProfile = (event, context, callback) => {
         if(undefined !== typeof element.bio && element.bio.length){
           let stringified_bio = '<!DOCTYPE html>'+`<div>${element.bio}</div>`;
           let dom = new JSDOM(stringified_bio);
-          stringified_bio = dom.window.document.querySelector("div").textContent.toString();
-          element.bio = stringified_bio;
+          let new_bio = '';
+          dom.window.document.querySelector("div").childNodes ? dom.window.document.querySelector("div").childNodes.forEach(node => {
+            new_bio = new_bio + '' + node.textContent.toString();
+          }) : '';
+          element.bio = new_bio;
         }
         resultarr.push({ _id: i.toString(), first_name: element.first_name, last_name: element.last_name, email: element.email, phone: element.phone, bio: element.bio, headshot_url_1: element.headshot_url_1, headshot_url_2: element.headshot_url_2, headshot_url_3: element.headshot_url_3, headshot_url_4: element.headshot_url_4, resume_url: element.resume_url, demo_reel_url: element.demo_reel_url });
         i++;
